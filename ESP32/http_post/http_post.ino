b5a5b882be49d31ea11e3059337dbe7356606fc4
dudaalmeida <duda.s.almeida10@gmail.com>
2024-12-26 18:32:37 -0300

    ESP32 exibindo inf.

--- ESP32/http_post/http_post.ino
+++ ESP32/http_post/http_post.ino
@@ -1,7 +1,11 @@
 #include <driver/i2s.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <ArduinoJson.h>
 
+// Definições do I2S
 #define I2S_SD 25
 #define I2S_WS 26
 #define I2S_SCK 33
@@ -13,17 +17,45 @@
 #define I2S_CHANNEL_NUM (1)
 #define SEND_BUFFER_SIZE (I2S_READ_LEN)
 
+// Definições do Display SSD1306
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET 4
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+};
+
+// Definições de Wi-Fi
 const char* ssid = "NET_2GE9DC82";
 const char* password = "DDE9DC82";
-
-//const char* serverUrl = "http://192.168.182.94:5000/upload";
 const char* serverUrl = "http://192.168.0.83:5000/upload";
 
 HTTPClient http;
 
 void setup() {
   Serial.begin(115200);
-  
+
+
+  // Inicializar o display
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("Erro ao inicializar o display SSD1306.");
+    while (true);
+  }
+
+  display.display();
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Iniciando...");
+  display.display();
+
   // Conectar ao Wi-Fi
   connectWiFi();
 
@@ -39,12 +71,24 @@ void loop() {
 }
 
 void connectWiFi() {
-  Serial.println("Conectando ao Wi-Fi...");
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Conectando ao Wi-Fi...");
+  display.display();
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Wi-Fi conectado!");
+  display.print("IP: ");
+  display.println(WiFi.localIP());
+  display.display();
+
   Serial.println("\nWi-Fi conectado!");
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
@@ -75,68 +119,154 @@ void i2sInit() {
   i2s_set_clk(I2S_PORT, I2S_SAMPLE_RATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
 }
 
+void getInferenceFromServer() {
+  if (WiFi.status() == WL_CONNECTED) {
+    String getUrl = "http://192.168.0.83:5000/infer";
+
+    http.begin(getUrl);
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.printf("Resposta do servidor (GET): %s\n", response.c_str());
+      
+      ParseJSON(httpResponseCode, response);
+    } else {
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Erro no GET!");
+      display.display();
+      Serial.printf("Erro ao realizar GET: %s\n", http.errorToString(httpResponseCode).c_str());
+    }
+
+    http.end();
+  } else {
+    display.clearDisplay();
+    display.println("Wi-Fi desconectado!");
+    display.display();
+    Serial.println("Erro: Wi-Fi desconectado!");
+  }
+}
+
 void i2s_adc_send(void *arg) {
   size_t bytes_read;
-  char* i2s_read_buff = (char*) malloc(I2S_READ_LEN);
-  
-  Serial.println("Iniciando gravação e envio...");
+  char* i2s_read_buff = (char*)malloc(I2S_READ_LEN);
 
-  for (int i = 0; i < RECORD_TIME; i++) { // Loop para RECORD_TIME segundos
-    // Ler dados do microfone I2S
-    i2s_read(I2S_PORT, (void*) i2s_read_buff, I2S_READ_LEN, &bytes_read, portMAX_DELAY);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Gravando audio...");
+  display.display();
 
-    // Enviar dados via HTTP
+  for (int i = 0; i < RECORD_TIME; i++) {
+    i2s_read(I2S_PORT, (void*)i2s_read_buff, I2S_READ_LEN, &bytes_read, portMAX_DELAY);
+
     sendAudioToServer((uint8_t*)i2s_read_buff, bytes_read);
-    Serial.printf("Enviado %u bytes para o servidor\n", bytes_read);
+    //display.clearDisplay();
+    //display.setCursor(0, 0);
+    //display.printf("Enviado: %u bytes", bytes_read);
+    //Serial.println("Enviado: %u bytes", bytes_read);
+    //display.display();
   }
-  
-  // Envio final com a flag JSON sinalizando o término
+
   sendEndFlag();
 
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Envio concluido!");
+  Serial.println("Envio concluido!");
+  display.display();
+
+  getInferenceFromServer();
+
   free(i2s_read_buff);
-  Serial.println("Gravação e envio concluídos.");
   vTaskDelete(NULL);
+}
+
+void ParseJSON(int httpResponseCode, String response){
+  Serial.printf("Código HTTP: %d\n", httpResponseCode);
+  Serial.println("Resposta do servidor:");
+  Serial.println(response);
+
+  // Parse do JSON
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, response);
+  if (error) {
+    Serial.print("Erro ao analisar JSON: ");
+    Serial.println(error.f_str());
+    display.println("Erro JSON!");
+  } else {
+    // Exibir campos relevantes do JSON
+    const char* status = doc["status"]; // Exemplo de chave "status"
+    const char* message = doc["message"]; // Exemplo de chave "message"
+        
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Servidor:");
+    //display.println(status);
+    Serial.printf("Status: %s, Message: %s\n", status, message);
+    display.println(message);
+    display.display();
+  }
 }
 
 void sendAudioToServer(uint8_t* data, size_t length) {
   if (WiFi.status() == WL_CONNECTED) {
-
     http.begin(serverUrl);
     http.addHeader("Content-Type", "application/octet-stream");
 
     int httpResponseCode = http.POST(data, length);
+    String response = http.getString();
 
     if (httpResponseCode > 0) {
-      Serial.printf("Resposta do servidor: %d\n", httpResponseCode);
+      Serial.printf("Código HTTP: %d\n", httpResponseCode);
+      Serial.println("Resposta do servidor:");
+      Serial.println(response);
+      
+      ParseJSON(httpResponseCode, response);
+
     } else {
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Erro ao enviar!");
+      display.display();
       Serial.printf("Erro ao enviar: %s\n", http.errorToString(httpResponseCode).c_str());
     }
+
     http.end();
   } else {
+    display.clearDisplay();
+    display.println("Wi-Fi desconectado!");
+    display.display();
     Serial.println("Erro: Wi-Fi desconectado!");
   }
 }
 
 void sendEndFlag() {
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-
-    Serial.println("Enviando sinalização de término...");
-
-    http.begin(serverUrl);  // URL do servidor
+    http.begin(serverUrl);
     http.addHeader("Content-Type", "application/json");
 
-    String jsonPayload = "{\"end\": true}";  // JSON com a flag de término
+    String jsonPayload = "{\"end\": true}";
     int httpResponseCode = http.POST(jsonPayload);
 
+    //display.clearDisplay();
+    //display.setCursor(0, 0);
+
     if (httpResponseCode > 0) {
-      Serial.printf("Sinalização de término enviada. Resposta: %d\n", httpResponseCode);
+      //display.println("Sinal enviado!");
+      //display.display();
+      Serial.printf("Sinal enviado: %d\n", httpResponseCode);
     } else {
-      Serial.printf("Erro ao enviar sinalização de término: %s\n", http.errorToString(httpResponseCode).c_str());
+      display.println("Erro sinal!");
+      display.display();
+      Serial.printf("Erro ao enviar sinal: %s\n", http.errorToString(httpResponseCode).c_str());
     }
 
     http.end();
   } else {
-    Serial.println("Erro: Wi-Fi desconectado ao tentar enviar sinalização de término!");
+    display.clearDisplay();
+    display.println("Wi-Fi desconectado!");
+    display.display();
+    Serial.println("Erro: Wi-Fi desconectado!");
   }
 }
--- server.py
+++ server.py
@@ -28,10 +28,24 @@ fragment_count = 0  # Contador de fragmentos
 
 classes = ["Alegria", "Desgosto", "Medo", "Neutro", "Raiva", "Surpresa", "Tristeza"]
 
-#model = load_model("C:/Users/eduarda.almeida/Desktop/Servidor_Flask_Modelo/modelo_emocoes.h5") 
-#model = load_model("C:/Users/eduarda.almeida/Desktop/Servidor_Flask_Modelo/modelo_emocoes_norm.h5")
-#model = load_model("C:/Users/eduarda.almeida/Desktop/Servidor_Flask_Modelo/modelo_nao_aguento_mais.h5") 
 model = load_model(MODEL_PATH)
+
+@app.route("/infer", methods=["GET"])
+def infer():
+    try:
+        # Realizar a inferência usando o áudio final processado
+        processed_audio = process_audio(FINAL_AUDIO)
+        predictions = model.predict(processed_audio)
+        predicted_class = np.argmax(predictions, axis=1)[0]
+        predicted_class_name = classes[predicted_class]
+
+        print(f"Predições: {predictions}")
+        print(f"Classe prevista: {predicted_class_name}")
+
+        return jsonify({"status": "success", "message": predicted_class_name}), 200
+    except Exception as e:
+        print(f"Erro durante a inferência: {str(e)}")
+        return jsonify({"status": "error", "message": "Erro durante a inferência"}), 500
     
 @app.route("/upload", methods=["POST"])
 def upload_audio():
@@ -57,7 +71,7 @@ def upload_audio():
                         os.remove(fragment_path)  # Remove fragmento compilado
 
             print(f"Áudio final salvo como '{FINAL_AUDIO}'")
-            fragment_count = 0  # Reinicia contador
+            fragment_count = 0 
 
             processed_audio = process_audio(FINAL_AUDIO)
             #processed_audio = process_audio(AUDIO_PATH)
@@ -69,7 +83,7 @@ def upload_audio():
             print(f"Predições: {predictions}")
             print(f"Classe prevista: {predicted_class_name}")
 
-            return jsonify({"status": "success", "message": "Áudio compilado", "class": f"{predicted_class_name}"})
+            return jsonify({"status": "success", "message": f"{predicted_class_name}"}), 200
 
     # Caso seja um fragmento de áudio comum
     file_path = os.path.join(TEMP_FOLDER, f"fragment_{fragment_count}.wav")
@@ -80,7 +94,7 @@ def upload_audio():
     print(f"Fragmento {fragment_count} recebido e salvo.")
 
     #print(audio_arrays)
-
+    
     return jsonify({"status": "success", "message": "Fragmento recebido"}), 200
 
 if __name__ == "__main__":
